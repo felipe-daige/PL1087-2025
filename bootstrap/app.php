5a831bb2df636b7dd7ef64cc9d810a82fa28bb9c
felipedaige@gmail.com <felipedaige@gmail.com>
2025-12-06 04:15:56 -0300

    debug: habilita APP_DEBUG e adiciona exception handler para diagnosticar erro 500

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +16,12 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustHosts(at: ['*']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (Throwable $e, $request) {
+            // Log erro no stderr para a Vercel capturar
+            error_log('Laravel Error: ' . $e->getMessage());
+            error_log('File: ' . $e->getFile() . ':' . $e->getLine());
+            error_log('Trace: ' . $e->getTraceAsString());
+            
+            return null; // Deixa Laravel tratar normalmente
+        });
     })->create();
